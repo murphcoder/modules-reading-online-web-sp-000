08c1f0e849979b17164123d0958c4f8539f769c7
Sean Murphy <filmmakersean@fastmail.net>
2019-08-10 00:43:21 +0000

    Automatically backed up by Learn

---
+++ lib/dance_module.rb
@@ -0,0 +1,18 @@
+module Dance
+  
+  def twirl
+    "I'm twirling!"
+  end
+  
+  def jump
+    "Look how high I'm jumping!"
+  end
+  
+  def pirouette
+    "I'm doing a pirouette"
+  end
+  
+  def take_a_bow
+    ""
+  
+end
